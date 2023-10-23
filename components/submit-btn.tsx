64209abfbe1316be2ac7cc8c9477d1dabb4a8323
Jeffrey Gumbs <jeffrey@secureauthorities.com>
2023-10-23 00:58:25 -0600

    testing update on vercel

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -1,9 +1,11 @@
+
+//@ts-ignore
 import React from 'react'
 import { FaPaperPlane } from 'react-icons/fa';
-import {experimental_useFormStatus as useFormStatus } from 'react-dom';
+import { useFormStatus } from "react-dom";
 
 export default function SubmitBtn() {
-    const {pending} = useFormStatus();
+  const { pending } = useFormStatus();
   return (
     <>
     <button type='submit' 
