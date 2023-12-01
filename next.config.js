67b9c79aecb2fa7c8568755c34f09521e6bc869c
Jeffrey Gumbs <jeffrey@secureauthorities.com>
2023-11-30 21:48:44 -0700

    updated site image formate

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     experimental: {
         serverActions: true
       },
+      images: {
+        formats: ['image/avif', 'image/webp'],
+      },
 }
 
 module.exports = nextConfig
